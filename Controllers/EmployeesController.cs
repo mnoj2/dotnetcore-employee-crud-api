7f66ea7401bae149d56f1da6756a7326ea993cfd
Manojkumar <manojkumarg240@example.com>
2025-08-31 21:45:50 +0530

    Initial commit - Employee CRUD API

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,92 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase {
+
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext context) {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees() {
+
+            var allEmployees = _context.Employees.ToList();
+
+            return Ok(allEmployees);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployee(Guid id) {
+
+            var Employee = _context.Employees.Find(id);
+
+            if(Employee is null) {
+                return NotFound();
+            }
+
+            return Ok(Employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto) {
+
+            var Employee = new Employee() {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+
+            _context.Employees.Add(Employee);
+            _context.SaveChanges();
+
+            return Ok(Employee);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto) {
+
+            var Employee = _context.Employees.Find(id);
+
+            if(Employee is null) {
+                return NotFound();
+            }
+
+            Employee.Name = updateEmployeeDto.Name;
+            Employee.Email = updateEmployeeDto.Email;
+            Employee.Phone = updateEmployeeDto.Phone;
+            Employee.Salary = updateEmployeeDto.Salary;
+
+            _context.SaveChanges();
+
+            return Ok(Employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id) {
+            
+            var Employee = _context.Employees.Find(id);
+
+            if(Employee is null) {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(Employee);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+
+using EmployeeAdminPortal.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
